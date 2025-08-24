4bc2de7278fd8330d8c3ba85f2c36ec98d758f42
mindyGit <mindysahar@gmail.com>
2025-08-24 21:28:19 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CapitalsMap from "./CapitalsMap";
+
+function App() {
+  return (
+    <div className="App">
+      <CapitalsMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CapitalsMap.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from "react";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import "@arcgis/core/assets/esri/themes/light/main.css";
+
+export default function CapitalsMap() {
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    const map = new Map({ basemap: "arcgis-topographic" });
+
+    const capitalsLayer = new FeatureLayer({
+      portalItem: { id: "d9677f2ef1d547c29fc30e628596f0c0" },
+      outFields: ["*"],
+      renderer: {
+        type: "simple",
+        symbol: {
+          type: "simple-marker",
+          style: "circle",
+          size: 8,
+          color: "red",
+          outline: { color: "white", width: 1 }
+        }
+      },
+      popupTemplate: {
+        title: "{CITY_NAME}",
+        content: "<b>Population:</b> {POP}"
+      }
+    });
+
+    map.add(capitalsLayer);
+
+    const view = new MapView({
+      container: mapDiv.current,
+      map,
+      center: [35, 31.5], // Israel
+      zoom: 6
+    });
+
+    return () => {
+      if (view) {
+        view.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>
+        World Capitals • ArcGIS JS • Israel Focus
+      </h2>
+      <div
+        ref={mapDiv}
+        id="mapDiv"
+        style={{ width: "100%", height: "80vh", border: "1px solid #ccc" }}
+      ></div>
+    </div>
+  );
+}
