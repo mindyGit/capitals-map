05bd36eb585ec9eed34a2d480253ee6b12b991ee
mindyGit <mindysahar@gmail.com>
2025-08-28 12:37:39 +0300

    Add dist folder

---
+++ assets/VertexSnappingCandidate-DTMF81Pp.js
@@ -0,0 +1,1 @@
+import{g as r}from"./hitTestSelectUtils-BfSIqm3n.js";import{n}from"./RouteLayerInteraction-CHr_M4lY.js";import{n as o}from"./PointSnappingHint-BdibPLBX.js";class m extends n{constructor(t){super({...t,constraint:new r(t.targetPoint)})}get hints(){return[new o(this.targetPoint,this.isDraped,this.domain)]}}export{m as r};
