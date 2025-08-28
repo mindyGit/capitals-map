9c31bc3ff0c59003be25e2687e587366d2902901
mindyGit <mindysahar@gmail.com>
2025-08-28 12:49:53 +0300

    return version

--- src/CapitalsMap.jsx
+++ src/CapitalsMap.jsx
@@ -5,55 +5,45 @@ import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import "@arcgis/core/assets/esri/themes/light/main.css";
 
 export default function CapitalsMap() {
-    const mapDiv = useRef(null);
+  const mapDiv = useRef(null);
 
-    useEffect(() => {
-        const map = new Map({ basemap: "arcgis-topographic" });
-      
-        const capitalsLayer = new FeatureLayer({
-          portalItem: { id: "d9677f2ef1d547c29fc30e628596f0c0" },
-          outFields: ["*"],
-          renderer: {
-            type: "simple",
-            symbol: {
-              type: "simple-marker",
-              style: "circle",
-              size: 8,
-              color: "red",
-              outline: { color: "white", width: 1 }
-            }
-          },
-          popupTemplate: {
-            title: "{CITY_NAME}",
-            content: "<b>Population:</b> {POP}"
-          }
-        });
-      
-        map.add(capitalsLayer);
-      
-        const view = new MapView({
-          container: mapDiv.current,
-          map,
-          center: [35, 31.5],
-          zoom: 10
-        });
-      
-        
-        capitalsLayer.queryFeatures().then((results) => {
-          if (results.features.length) {
-            view.popup.open({
-              features: [results.features[0]],
-              location: results.features[0].geometry
-            });
-          }
-        });
-      
-        return () => {
-          if (view) {
-            view.destroy();
-          }
-        };
-      }, []);
+  useEffect(() => {
+    const map = new Map({ basemap: "arcgis-topographic" });
+
+    const capitalsLayer = new FeatureLayer({
+      portalItem: { id: "d9677f2ef1d547c29fc30e628596f0c0" },
+      outFields: ["*"],
+      renderer: {
+        type: "simple",
+        symbol: {
+          type: "simple-marker",
+          style: "circle",
+          size: 8,
+          color: "red",
+          outline: { color: "white", width: 1 }
+        }
+      },
+      popupTemplate: {
+        title: "{CITY_NAME}",
+        content: "<b>Population:</b> {POP}"
+      }
+    });
+
+    map.add(capitalsLayer);
+
+    const view = new MapView({
+      container: mapDiv.current,
+      map,
+      center: [35, 31.5], // Israel
+      zoom: 10
+    });
+
+    return () => {
+      if (view) {
+        view.destroy();
+      }
+    };
+  }, []);
       
 
     return (
